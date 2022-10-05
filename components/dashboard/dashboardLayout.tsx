ff137f704bd429445b78d8a2c9969f3fe40eba3b
Louis Nyirongo <louisnyirongo73@gmail.com>
2022-10-05 15:22:59 +0200

    iframe added

--- components/dashboard/dashboardLayout.tsx
+++ components/dashboard/dashboardLayout.tsx
@@ -1,8 +1,25 @@
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Paper, useMediaQuery, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
 import DesktopView from "./responsiveness/desktopView";
 import MobileView from "./responsiveness/mobileView";
 export default function DashboardLayout() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("lg"));
-  return matches ? <DesktopView /> : <MobileView />;
+  return (
+    <>
+      {matches ? <DesktopView /> : <MobileView />}
+
+      <Box
+        sx={{ px: `${matches ? "100px" : "0px"}`, mt: "20px", width: "100%" }}
+      >
+        <iframe
+          id="inlineFrameExample"
+          title="Inline Frame Example"
+          width="100%"
+          height="800px"
+          src="https://phylodynamics2.pandemicprepardness.org/SARS-CoV-2/Malawi"
+        ></iframe>
+      </Box>
+    </>
+  );
 }
